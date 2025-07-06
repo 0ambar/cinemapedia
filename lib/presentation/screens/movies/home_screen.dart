4255c618d324a05dc40f909c832974b2d3f7c842
0ambar <abrahamr677@gmail.com>
2025-07-06 10:41:59 -0600

    feat: showing fetched movies list from TheMovieDB-API

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -15,16 +16,39 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class _HomeView extends StatefulWidget {
+class _HomeView extends ConsumerStatefulWidget {
   const _HomeView();
 
   @override
-  State<_HomeView> createState() => _HomeViewState();
+  _HomeViewState createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<_HomeView> {
+
+// Consumer has scope to 'state'
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    if(nowPlayingMovies.length == 0) return Center(child: CircularProgressIndicator(strokeWidth: 6.0,));
+    
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      }
+    );
   }
 }
