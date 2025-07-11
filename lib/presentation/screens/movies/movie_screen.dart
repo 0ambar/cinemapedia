0cc675cccb1f1a1f52ea7ac6fc486661dba28d8e
0ambar <abrahamr677@gmail.com>
2025-07-11 11:12:27 -0600

    feat: movie screen finished

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,5 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/actor.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -39,6 +41,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
         slivers: [
 
           _CustomSliverAppBar(movie: movie,),
+
           SliverList(delegate: SliverChildBuilderDelegate(
             (context, index) => _MovieDetails(movie: movie),
             childCount: 1
@@ -60,6 +63,7 @@ class _CustomSliverAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final size = MediaQuery.of(context).size;
+
     return SliverAppBar(
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
@@ -78,6 +82,10 @@ class _CustomSliverAppBar extends StatelessWidget {
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if( loadingProgress != null) return const SizedBox();
+                  return FadeIn(child: child);
+                },
               ),
             ),
 
@@ -100,11 +108,13 @@ class _CustomSliverAppBar extends StatelessWidget {
             const SizedBox.expand(
               child: DecoratedBox(
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.3],
+                  gradient: RadialGradient(
+                    // begin: Alignment.topLeft,
+                    radius: 2,
+                    center: Alignment.topLeft,
+                    stops: [0.0, 0.2],
                     colors: [
-                      Colors.black87,
+                      Colors.black54,
                       Colors.transparent,
                     ]
                   )
@@ -185,8 +195,72 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        const SizedBox(height: 100,)
+        _ActorsByMovie(movieId: movie.id.toString()),
+
+        const SizedBox(height: 80,)
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final List<Actor>? actors = ref.watch( actorsByMovieProvider )[movieId];
+
+    if( actors == null) return const CircularProgressIndicator(strokeWidth: 2,);
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        itemCount: actors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: FadeInRight(
+              child: Column(
+              
+                children: [
+                  
+                  // Actor photo
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+              
+                  // Actor name
+                  const SizedBox(height: 5,),
+                  Text(actor.name, maxLines: 2,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(actor.character ?? '', 
+                    maxLines: 2,
+                    style: const TextStyle( fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis,),
+                    textAlign: TextAlign.center,
+                  )
+              
+                ],
+              ),
+            ),
+          );
+        }
+      ),
     );
   }
 }
